883890ad9aa01eaf0610762125bf8f909e046b88
potassiummmm <54800242+potassiummmm@users.noreply.github.com>
2021-02-09 13:59:10 +0800

    Update bilibili subscripion plugin

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-BOT_ACCOUNT: int = 0
-BOT_HOST: str = ""
-AUTHKEY: str = ""
+from collections import defaultdict
+
+BOT_ACCOUNT: int = 1521138307
+BOT_HOST: str = "http://localhost:8081"
+AUTHKEY: str = "testauthkey"
 MYSQL_HOST: str = ""
 MYSQL_PORT: str = ""
 MYSQL_USER: str = ""
@@ -11,3 +13,4 @@ SETU_API_KEY: str = ""
 SETU_ENABLED_GROUPS: set = set()
 ADMIN_QQ: int = 0
 LEETCODE_ENABLED_GROUPS: set = set()
+BILIBILI_SUBSCRIBE_SETTINGS: dict = defaultdict(set)
--- plugins/bilibili.py
+++ plugins/bilibili.py
@@ -1,5 +1,6 @@
 from graia.application.entry import GraiaMiraiApplication, GroupMessage, MessageChain, Plain, Group
 from graia.scheduler.timers import every_custom_seconds
+from config import BILIBILI_SUBSCRIBE_SETTINGS
 import aiohttp
 import json
 import time
@@ -12,6 +13,16 @@ async def getUserInfoJson(uuid: int) -> dict:
         async with session.get(url) as resp:
             data_json = await resp.json()
             return data_json
+
+
+async def judgeUuid(uuid: int) -> tuple:
+    url = "https://api.bilibili.com/x/space/acc/info?mid=%d" % uuid
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            data_json = await resp.json()
+            return (True,
+                    data_json["data"]["name"]) if data_json["code"] == 0 else (
+                        False, data_json["message"])
 
 
 async def getLatestVideoTimestamp(uuid: int) -> int:
@@ -37,15 +48,13 @@ bcc = Instance.bcc()
 
 @sche.schedule(every_custom_seconds(5))
 async def bilibili_subscribe_scheduler():
-    ltt_newest_timestamp = await getLatestVideoTimestamp(12434430)
-    van_newest_timestamp = await getLatestVideoTimestamp(23604445)
-    if time.time() - ltt_newest_timestamp < 5:
-        result = await getLatestVideoInfo(12434430)
-        await app.sendGroupMessage(546091207,
-                                   MessageChain.create([Plain(result)]))
-    if time.time() - van_newest_timestamp < 5:
-        await app.sendGroupMessage(434499605,
-                                   MessageChain.create([Plain(result)]))
+    for group in BILIBILI_SUBSCRIBE_SETTINGS:
+        for up in BILIBILI_SUBSCRIBE_SETTINGS[group]:
+            latest_timestamp = await getLatestVideoTimestamp(up)
+            if time.time() - latest_timestamp < 5:
+                result = await getLatestVideoInfo(up)
+                await app.sendGroupMessage(
+                    group, MessageChain.create([Plain(result)]))
 
 
 @bcc.receiver("GroupMessage")
@@ -53,5 +62,36 @@ async def bilibili_subscribe_auth_listener(app: GraiaMiraiApplication,
                                            group: Group,
                                            message: MessageChain):
     if message.asDisplay().startswith("订阅") and len(
-            message.asDisplay().split(' ')):
-        pass
+            message.asDisplay().split(' ')) == 2:
+        try:
+            uuid = int(message.asDisplay().split(' ')[1])
+            res = await judgeUuid(uuid)
+            msg = MessageChain.create([Plain(res[1])])
+            if res[0]:  # 请求成功
+                print("请求成功")
+                if (uuid in BILIBILI_SUBSCRIBE_SETTINGS[group.id]):
+                    msg.plus(MessageChain.create([Plain("已订阅 请勿重复操作")]))
+                else:
+                    BILIBILI_SUBSCRIBE_SETTINGS[group.id].add(uuid)
+                    msg.plus(MessageChain.create([Plain(" 订阅成功!")]))
+            await app.sendGroupMessage(group, msg)
+        except ValueError:
+            msg = MessageChain.create(Plain("请输入整数uuid"))
+            await app.sendGroupMessage(group, msg)
+
+    if message.asDisplay().startswith("取消订阅") and len(
+            message.asDisplay().split(' ')) == 2:
+        try:
+            uuid = int(message.asDisplay().split(' ')[1])
+            res = await judgeUuid(uuid)
+            msg = MessageChain.create([Plain(res[1])])
+            if res[0]:  # 请求成功
+                if (uuid in BILIBILI_SUBSCRIBE_SETTINGS[group.id]):
+                    BILIBILI_SUBSCRIBE_SETTINGS[group.id].remove(uuid)
+                    msg.plus(MessageChain.create([Plain(" 取消订阅成功!")]))
+                else:
+                    msg.plus(MessageChain.create([Plain("未订阅 无法取消")]))
+            await app.sendGroupMessage(group, msg)
+        except ValueError:
+            msg = MessageChain.create(Plain("请输入整数uuid"))
+            await app.sendGroupMessage(group, msg)
